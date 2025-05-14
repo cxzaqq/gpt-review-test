97c0ca77c7d0e28c9030c4faa91c99a1d4b6439d
cxzaqq <roy1228@naver.com>
2025-05-14 09:53:01 +0900

    first commit

---
+++ src/main/java/com/example/gptreviewtest/GptReviewTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.gptreviewtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GptReviewTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GptReviewTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gptreviewtest/GptReviewTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gptreviewtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GptReviewTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
