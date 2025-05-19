56ab6b32013ff0d9dd1d920d543adcc95a56d675
cxzaqq <roy1228@naver.com>
2025-05-20 08:48:51 +0900

    테이블 세팅 수정 및 초기 데이터 설정

---
+++ sql/init_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO `department`
+(`dept_code`, `name`)
+VALUES
+("DPT_PUR", "구매팀"),
+("DPT_SAL", "영업팀"),
+("DPT_LOG", "물류팀")
--- sql/table_setting.sql
+++ sql/table_setting.sql
@@ -6,14 +6,24 @@ DROP TABLE IF EXISTS `user_role`;
 DROP TABLE IF EXISTS `menu`;
 DROP TABLE IF EXISTS `role`;
 DROP TABLE IF EXISTS `user`;
+DROP TABLE IF EXISTS `department`;
+
+-- 부서 테이블
+CREATE TABLE `department` (
+  `dept_code` VARCHAR(50) PRIMARY KEY
+, `name` VARCHAR(100) NOT NULL
+);
 
 -- 회원 테이블
 CREATE TABLE `user` (
-  `id` VARCHAR(50) PRIMARY KEY
+  `user_code` VARCHAR(50) PRIMARY KEY
 , `password` VARCHAR(100) NOT NULL
 , `name` VARCHAR(100) NOT NULL
 , `phone` VARCHAR(20) UNIQUE NOT NULL
 , `email` VARCHAR(100) UNIQUE NOT NULL
+, `user_type` VARCHAR(50) NOT NULL CHECK (user_type IN ('HQ_EMPLOYEE', 'FRANCHISE_OWNER', 'SUPPLIER'))
+, `dept_code` VARCHAR(50)
+, FOREIGN KEY (`dept_code`) REFERENCES `department`(`dept_code`)
 );
 
 -- 역할 테이블
@@ -24,10 +34,10 @@ CREATE TABLE `role` (
 
 -- 회원_역할 매핑 테이블
 CREATE TABLE `user_role` (
-  `user_id` VARCHAR(50)
+  `user_code` VARCHAR(50)
 , `role_code` VARCHAR(50)
-, PRIMARY KEY (`user_id`, `role_code`)
-, FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE CASCADE
+, PRIMARY KEY (`user_code`, `role_code`)
+, FOREIGN KEY (`user_code`) REFERENCES `user`(`user_code`) ON DELETE CASCADE
 , FOREIGN KEY (`role_code`) REFERENCES `role`(`role_code`) ON DELETE CASCADE
 );
 
@@ -48,7 +58,7 @@ CREATE TABLE `role_menu` (
 , FOREIGN KEY (`menu_code`) REFERENCES `menu`(`menu_code`) ON DELETE CASCADE
 );
 
--- 메뉴 별 기능 코드 정의
+-- 메뉴 별 기능 코드 정의 테이블
 CREATE TABLE `menu_func` (
   `menu_code` VARCHAR(50)
 , `func_code` VARCHAR(50)
@@ -56,7 +66,7 @@ CREATE TABLE `menu_func` (
 , FOREIGN KEY (`menu_code`) REFERENCES `menu`(`menu_code`) ON DELETE CASCADE
 );
 
--- 역할_메뉴 별 기능 코드 매핑
+-- 역할_메뉴 별 기능 코드 매핑 테이블
 CREATE TABLE `role_menu_func` (
   `role_code` VARCHAR(50)
 , `menu_code` VARCHAR(50)
