0d23fbe7f44379c151ba36a522395b80a16b42f2
cxzaqq <roy1228@naver.com>
2025-05-21 21:01:33 +0900

    user enums 추가

---
+++ src/main/java/com/example/gptreviewtest/user/enums/DeptCode.java
@@ -0,0 +1,7 @@
+package com.example.gptreviewtest.user.enums;
+
+public enum DeptCode {
+    DPT_PUR,
+    DPT_SAL,
+    DPT_LOG
+}
---
+++ src/main/java/com/example/gptreviewtest/user/enums/UserType.java
@@ -0,0 +1,8 @@
+package com.example.gptreviewtest.user.enums;
+
+public enum UserType {
+    ADMIN,
+    HQ,
+    FRANCHISE,
+    SUPPLIER
+}
