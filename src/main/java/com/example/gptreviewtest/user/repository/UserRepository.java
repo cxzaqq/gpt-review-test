12fcacfbb98e9b007151a8e50ff63c5787b9dd11
cxzaqq <roy1228@naver.com>
2025-05-27 08:59:01 +0900

    email 발송

--- src/main/java/com/example/gptreviewtest/user/repository/UserRepository.java
+++ src/main/java/com/example/gptreviewtest/user/repository/UserRepository.java
@@ -10,6 +10,12 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, String> {
 
+    // 전화번호 존재 여부
+    boolean existsByPhone(String phone);
+
+    // 이메일 존재 여부
+    boolean existsByEmail(String email);
+
     // userType과 userCodePrefix로 시작하는 코드 중 가장 최신 코드 1건 조회
     Optional<UserEntity> findTopByTypeAndCodeStartingWithOrderByCodeDesc(UserType userType, String userCodePrefix);
 }
